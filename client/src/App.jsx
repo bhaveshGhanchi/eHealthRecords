7e968c6f4f362410a1065fba66f578e3db8f506a
bhaveshGhanchi <bhaveshghanchi27@gmall.com>
2023-04-30 20:11:07 +0530

    Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,54 @@
-import {BrowserRouter as Router, Route,Routes, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
 import './App.css';
-import  { useReducer, useState, useEffect } from 'react';
-import { Provider } from './components/Store/Context';
-import { initialState, reducer } from './components/Store/userReduce';
-import Home from './components/Home/Home'
-import Patient from './components/Patient/Patient'
-import AssignedPat from './components/Patient/PatAssign';
-import Profile from './components/Doctor/Profile';
-import AllDoc from './components/Foradmin/AllDoc';
-import AllPat from './components/Foradmin/AllPat';
-import PatRep from './components/PatientReports/PatRep';
+import { useReducer, useState, useEffect } from 'react';
+import { Provider } from './pages/Store/Context';
+import { initialState, reducer } from './pages/Store/userReduce';
+import Home from './pages/Home/Home'
+import Patient from './pages/Patient/Patient'
+import AssignedPat from './pages/Patient/PatAssign';
+import Profile from './pages/Doctor/Profile';
+import AllDoc from './pages/Foradmin/AllDoc';
+import AllPat from './pages/Foradmin/AllPat';
+import PatRep from './pages/PatientReports/PatRep';
 import { SnackbarProvider } from 'notistack';
-
+import { HelmetProvider } from 'react-helmet-async';
+// routes
+import DashboardAppPage from './pages/Dashboard/Dashboard';
+// theme
+import ThemeProvider from './theme';
+// components
+import { StyledChart } from './components/chart';
+import ScrollToTop from './components/scroll-to-top';
 
 function App() {
-  const [state,dispatch] = useReducer(reducer,initialState)
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-          <SnackbarProvider maxSnack={3}>
-      <Provider value={{state,dispatch}}>
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} /> 
-          <Route exact path='/Patient/:id' element={<Patient />} /> 
-          <Route exact path='/Doctor' element={<Profile />} /> 
-          <Route exact path='/Patients' element={<AssignedPat />} />
-          <Route exact path='/AllDoctor' element={<AllDoc />} /> 
-          <Route exact path='/AllPatient' element={<AllPat />} /> 
-          <Route exact path='/PatientReport' element={<PatRep />} />
+      <HelmetProvider>
 
-          
-        </Routes>  
-      </Router> 
-      </Provider>
-          </SnackbarProvider>
+        <SnackbarProvider maxSnack={3}>
+          <Provider value={{ state, dispatch }}>
+            <Router>
+              <ThemeProvider>
+                <ScrollToTop />
+                <StyledChart />
+                <Routes>
+                  <Route exact path='/' element={<Home />} />
+                  <Route exact path='/Patient/:id' element={<Patient />} />
+                  <Route exact path='/Doctor' element={<Profile />} />
+                  <Route exact path='/Patients' element={<AssignedPat />} />
+                  <Route exact path='/AllDoctor' element={<AllDoc />} />
+                  <Route exact path='/AllPatient' element={<AllPat />} />
+                  <Route exact path='/PatientReport' element={<PatRep />} />
+                  <Route exact path='/Dashboard' element={<DashboardAppPage/>} />
+
+                </Routes>
+              </ThemeProvider>
+            </Router>
+          </Provider>
+        </SnackbarProvider>
+
+      </HelmetProvider>
     </div>
   );
 }
