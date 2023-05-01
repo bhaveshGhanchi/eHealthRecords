c807d7f425969d739f49cc2fbf1a3a0de9e2e8d2
ruta1402 <ruta.kulkarni@somaiya.edu>
2023-05-01 10:50:50 +0530

    flask

---
+++ flask/app.py
@@ -0,0 +1,25 @@
+from flask import Flask,jsonify
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+
+
+client=MongoClient("mongodb+srv://test:test@cluster0.mbaqegg.mongodb.net/?retryWrites=true&w=majority")
+db=client["EHR"]
+userData=db["userdatas"]
+data = list(userData.find())
+df = pd.DataFrame(data)
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/report')
+def report():
+    reports=df['reports']
+    lst=reports.iloc[0]
+    return lst[0]
